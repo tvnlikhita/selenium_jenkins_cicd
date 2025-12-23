5198b5d698f017b752d3368f459cc6c9265481cf
tvnlikhita <nagalikhitatelanakula@gmail.com>
2025-12-23 18:44:35 +0530

    selenium code for dropdown feature

--- test_dropdown.py
+++ test_dropdown.py
@@ -2,16 +2,10 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 import time
-from selenium.webdriver.chrome.options import Options
 
-options = Options()
-options.add_argument("--headless=new")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-
-browser = webdriver.Chrome(options=options)
+browser = webdriver.Chrome()
 browser.get("https://practice.expandtesting.com/dropdown")
-# browser.maximize_window()
+browser.maximize_window()
 
 choose = Select(browser.find_element(By.ID, "dropdown"))
 choose.select_by_visible_text("Option 1")
